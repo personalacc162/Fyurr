0a6a9412ed96fc6ff794e271c9c0c5188173cd6d
personalacc162 <66521675+personalacc162@users.noreply.github.com>
2022-02-22 23:34:04 -0500

    first commit.

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -10,4 +10,4 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://Pycharm:pycharm@localhost:5432/Fyurr'
